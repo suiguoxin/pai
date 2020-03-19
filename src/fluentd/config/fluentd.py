424c1bd886fa571b868904335321aa7bd3cece0c
hzy46 <362583303@qq.com>
2020-03-19 17:12:10 +0800

    fix

--- src/fluentd/config/fluentd.py
+++ src/fluentd/config/fluentd.py
@@ -1,29 +1,19 @@
 #!/usr/bin/env python
-import copy
 import logging
 
 
 class Fluentd(object):
     def __init__(self, cluster_conf, service_conf, default_service_conf):
-        self.cluster_conf = cluster_conf
         self.logger = logging.getLogger(__name__)
 
-    @staticmethod
-    def merge_service_configuration(overwrite_srv_cfg, default_srv_cfg):
-        if overwrite_srv_cfg is None:
-            return default_srv_cfg
-        srv_cfg = default_srv_cfg.copy()
-        for k in overwrite_srv_cfg:
-            srv_cfg[k] = overwrite_srv_cfg[k]
-        return srv_cfg
-
     def validation_pre(self):
-        print(self.cluster_conf)
         return True, None
 
     def run(self):
         return {}
 
     def validation_post(self, conf):
-        print(conf)
+        if 'job-history' in conf['cluster']['common'] and conf['cluster']['common']['job-history'] is True:
+            if conf['postgresql']['enable'] is False or conf['internal-storage']['enable'] is False:
+                return False, "You must enable internal-storage and postgresql to use job history."
         return True, None
